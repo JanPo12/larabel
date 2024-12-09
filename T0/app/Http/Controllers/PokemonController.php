1496edf0106cd9ffbfd0a47d32114413d8fe789e
JanPons <jpons4@xarxa.fedac.cat>
2024-12-09 16:36:23 +0100

    Hola

---
+++ T0/app/Http/Controllers/PokemonController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pokemon;
+use Illuminate\Http\JsonResponse;
+
+class PokemonController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $data = Pokemon::with('pokemon_tipus')->with('pokemon_habilitats')->get(); 
+        return response()->json(['success' => true, 'data' => $data]);
+    }
+    public function show($id): JsonResponse
+    {
+        $data = Pokemon::where('id', '=', $id)->with('pokemon_tipus')->with('pokemon_habilitats')->get();
+        return response()->json(['success' => true, 'data' => $data]);
+    }
+}
+
+
